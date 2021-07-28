62a1da3c26fe1cd167755af527233b1525d85f0d
leadmeout <markhorvat@protonmail.com>
2021-07-28 17:51:48 +0200

    include wild card for allowed hosts

--- testsiteone/pages_project/pages_project/settings.py
+++ testsiteone/pages_project/pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-myod7_j*7ds)&lwd0@1u5&+twss!d^65^!1vbvgto(64tpoxc4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
